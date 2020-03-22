c1a8af841d26487036b7830b7d22fa1d3a152efc
MathDodo <mathias.h.larsen@jp.dk>
2020-03-22 14:13:58 +0100

    22-03-2020 14:13:57

--- Assets/HephaestusForge/Editor/AutoGitSave/AutoGitStartTimerProcessor.cs
+++ Assets/HephaestusForge/Editor/AutoGitSave/AutoGitStartTimerProcessor.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Linq;
+using UnityEditor;
 
-public class AutoGitStartTimerProcessor : MonoBehaviour
+namespace HephaestusForge.AutoGit
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AutoGitStartTimerProcessor : AssetPostprocessor
     {
-        
-    }
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            if (!importedAssets.Any(s => s.Contains("AutoGitTimerAndHistory")) && !deletedAssets.Any(s => s.Contains("AutoGitTimerAndHistory")) &&
+                !movedAssets.Any(s => s.Contains("AutoGitTimerAndHistory")) && !movedFromAssetPaths.Any(s => s.Contains("AutoGitTimerAndHistory")))
+            {
+                var guids = AssetDatabase.FindAssets("t:AutoGitTimerAndHistory");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+                if (guids.Length == 1)
+                {
+                    var autoGitTimer = AssetDatabase.LoadAssetAtPath<AutoGitTimerAndHistory>(AssetDatabase.GUIDToAssetPath(guids[0]));
+                    EditorApplication.delayCall += () => autoGitTimer.EditorInit();
+                }
+            }
+        }
     }
 }
